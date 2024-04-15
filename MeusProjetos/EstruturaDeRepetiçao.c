cb6994af006c27d2b46a4c6e7c05b25ec430c23f
RuanVNBezerra <ruanvictorbrazoka@gmail.com>
2024-04-15 09:14:39 -0300

    Estrutura De Repetição

---
+++ MeusProjetos/EstruturaDeRepetiçao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Inicializa a variável de controle
+    int contador = 1;
+
+    // Loop para imprimir os números de 1 a 10
+    while (contador <= 10) {
+        printf("Número: %d\n", contador);
+        contador++; // Incrementa o contador
+    }
+
+    return 0;
+}
